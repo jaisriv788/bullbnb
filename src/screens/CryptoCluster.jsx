31ea7be1cb09ef3791e633f73a8929e920f58a6e
jaisriv788 <srivastavajai845@gmail.com>
2025-10-06 15:36:15 +0530

    sperated the imports and data from the cryptocluster.jsx to cryptocluster.js in the data folder for easy maintence

--- src/screens/CryptoCluster.jsx
+++ src/screens/CryptoCluster.jsx
@@ -1,20 +1,7 @@
 import Footer from "../component/Footer";
 import { useSelector } from "react-redux";
 import logoSrc from "../assets/cryptocluster/CryptoCluster-logo.png";
-import book1 from "../assets/cryptocluster/vol-1.jpg";
-import book2 from "../assets/cryptocluster/vol-2.jpg";
-import book3 from "../assets/cryptocluster/vol-3.jpg";
-import book4 from "../assets/cryptocluster/vol-4.jpg";
-import book5 from "../assets/cryptocluster/vol-5.jpg";
-import book6 from "../assets/cryptocluster/vol-6.jpg";
-import book7 from "../assets/cryptocluster/vol-7.jpg";
-import book8 from "../assets/cryptocluster/vol-8.jpg";
-import book9 from "../assets/cryptocluster/vol-9.jpg";
-import book10 from "../assets/cryptocluster/vol-10.jpg";
-import book11 from "../assets/cryptocluster/vol-11.jpg";
-import book12 from "../assets/cryptocluster/vol-12.jpg";
-import book13 from "../assets/cryptocluster/vol-13.jpg";
-// import book14 from "../assets/cryptocluster/vol-14.jpg";
+import { data } from "../data/cryptocluster";
 
 function CryptoCluster({ openSidebar }) {
   const walletAddress = useSelector(
@@ -23,23 +10,6 @@ function CryptoCluster({ openSidebar }) {
   const currentWalletAddress = useSelector(
     (state) => state.accountDetails.walletAddress
   );
-
-  const data = [
-    { img: book1, link: "https://bullbnb.com/cryptocluster/A9D3Z7QX1" },
-    { img: book2, link: "https://bullbnb.com/cryptocluster/M4P8T2KN2" },
-    { img: book3, link: "https://bullbnb.com/cryptocluster/R7W6X0BD3" },
-    { img: book4, link: "https://bullbnb.com/cryptocluster/Q2J9Y5MT4" },
-    { img: book5, link: "https://bullbnb.com/cryptocluster/T3L4G8NZ5" },
-    { img: book6, link: "https://bullbnb.com/cryptocluster/H6U2K1VM6" },
-    { img: book7, link: "https://bullbnb.com/cryptocluster/B1C9S7EQ7" },
-    { img: book8, link: "https://bullbnb.com/cryptocluster/N8X5V4JW8" },
-    { img: book9, link: "https://bullbnb.com/cryptocluster/V3R2M6YZ9" },
-    { img: book10, link: "https://bullbnb.com/cryptocluster/D5F7W1PU10" },
-    { img: book11, link: "https://bullbnb.com/cryptocluster/Y6K3L9HT11" },
-    { img: book12, link: "https://bullbnb.com/cryptocluster/Z2N4B8QW12" },
-    { img: book13, link: "https://bullbnb.com/cryptocluster/C1J8U5MP13" },
-    // { img: book14, link: "https://bullbnb.com/cryptocluster/W7E0A2ZR14" },
-  ];
 
   return (
     <div
